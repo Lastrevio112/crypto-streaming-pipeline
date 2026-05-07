2bf0b43beae5ebffa6b751f9070763099397186f
Lastrevio <stefanzingzing@yahoo.com>
2026-05-07 19:30:21 +0000

    Fixed hashing function of custom partitioned to make it deterministic.

--- src/kafka_producers/custom_partitioner.py
+++ src/kafka_producers/custom_partitioner.py
@@ -1,5 +1,6 @@
 """ We will split our topics into 10 partitions based on the hash of the cryptocurrency.
     Five most active coins will get their own partition, the other 45 coins will be distributed across the remaining five."""
+import hashlib
 
 def custom_partitioner(key: str, num_partitions: int):
     PARTITION_MAP = {
@@ -15,5 +16,8 @@ def custom_partitioner(key: str, num_partitions: int):
         return PARTITION_MAP[key]
     
     # hash remaining keys into the leftover partition range
+    hash_bytes = hashlib.md5(key.encode()).digest()
+    hash_int = int.from_bytes(hash_bytes, 'big')
     fallback_range = num_partitions - FALLBACK_START
-    return FALLBACK_START + (hash(key) % fallback_range)
+    
+    return FALLBACK_START + (hash_int % fallback_range)
