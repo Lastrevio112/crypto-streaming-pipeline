32a45ec6a52206008f75a8fc0e842a2b75970b28
Lastrevio <stefanzingzing@yahoo.com>
2026-05-11 15:07:53 +0000

    added useful queries to file

---
+++ src/clickhouse/useful_queries.sql
@@ -0,0 +1,38 @@
+/* Nothing runs in production here - there are useful debugging or development queries that can be copy-pasted into Clickhouse's UI */
+
+--Size of each table on disk:
+SELECT
+    table,
+    formatReadableSize(sum(bytes_on_disk)) AS size
+FROM system.parts
+WHERE active
+GROUP BY table
+ORDER BY sum(bytes_on_disk) DESC;
+
+--Number of records per hour per table (change table name as you wish):
+SELECT 
+    dateName('day', _ingested_at) AS ingestedDate,
+    dateName('hour', _ingested_at) AS ingestedHour,
+    COUNT(*)
+FROM src_5s_sliding
+GROUP BY dateName('day', _ingested_at), dateName('hour', _ingested_at)
+ORDER BY 1 ASC, 2 ASC;
+
+--How much data on disk is added in the last 2 hours:
+SELECT
+    toStartOfHour(event_time) AS hour,
+    formatReadableSize(sum(size_in_bytes)) AS disk_size_added,
+    sum(rows) AS rows_added
+FROM system.part_log
+WHERE table = 'src_5s_sliding'
+  AND event_type = 'NewPart' -- Only count initial writes, not merges
+  AND event_time > now() - INTERVAL 2 HOUR
+GROUP BY hour
+ORDER BY hour DESC;
+
+--How much data is late:
+SELECT
+    avg(dateDiff('millisecond', window_end, _ingested_at)) / 1000.0 AS avg_diff_seconds,
+    quantiles(0.25, 0.5, 0.75, 0.9)(dateDiff('millisecond', window_end, _ingested_at) / 1000.0) AS quantiles
+FROM src_5s_sliding
+WHERE _ingested_at >= now64(3) - INTERVAL 1 HOUR;
